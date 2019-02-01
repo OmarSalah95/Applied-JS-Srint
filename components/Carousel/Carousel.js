c64ac4bc873af512e5628cc951af7686331a71d1
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-01 10:39:12 -0800

    Stretch is now functional.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,17 +4,28 @@ class Carousel {
       this.leftBtn = carousel.querySelector(".left-button");
       this.rightBtn = carousel.querySelector(".right-button");
       this.imgList = carousel.querySelectorAll("img");
+      this.index = 0
+      this.imgList[this.index].style.display = 'block'
       this.leftBtn.addEventListener("click", () => this.cycleLeft());
       this.rightBtn.addEventListener("click", () => this.cycleRight());
       
     }
     cycleLeft () {
+    this.imgList.forEach(img => img.style.display = "none")
+    this.index === 0
+        ? this.index = this.imgList.length-1
+        : this.index--
+    this.imgList[this.index].style.display = "block";
  
   
     }
   
     cycleRight () {
-      
+      this.imgList[this.index].style.display = "none";
+      this.index == this.imgList.length - 1
+        ? this.index = 0
+        : this.index ++;
+      this.imgList[this.index].style.display = "block";
     }
     
   }

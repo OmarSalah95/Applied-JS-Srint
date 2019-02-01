db8091e86076a94359596fe116e8c82094c2fcb7
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-01 11:13:30 -0800

    I had some issues i resolved when importing GSAP now figureing the best way to implement the slide in and out functionality of the imgs otherwise stretch is complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,31 +1,31 @@
 class Carousel {
     constructor (carousel) {
-      this.carousel = carousel;
-      this.leftBtn = carousel.querySelector(".left-button");
-      this.rightBtn = carousel.querySelector(".right-button");
-      this.imgList = carousel.querySelectorAll("img");
-      this.index = 0
-      this.imgList[this.index].style.display = 'block'
-      this.leftBtn.addEventListener("click", () => this.cycleLeft());
-      this.rightBtn.addEventListener("click", () => this.cycleRight());
+        this.carousel = carousel;
+        this.leftBtn = carousel.querySelector(".left-button");
+        this.rightBtn = carousel.querySelector(".right-button");
+        this.imgList = carousel.querySelectorAll("img");
+        this.index = 0
+        this.imgList[this.index].style.display = 'block'
+        this.leftBtn.addEventListener("click", () => this.cycleLeft());
+        this.rightBtn.addEventListener("click", () => this.cycleRight());
       
     }
+    
     cycleLeft () {
-    this.imgList.forEach(img => img.style.display = "none")
-    this.index === 0
+        this.imgList.forEach(img => img.style.display = "none")
+        this.index === 0
         ? this.index = this.imgList.length-1
         : this.index--
-    this.imgList[this.index].style.display = "block";
- 
-  
-    }
+        this.imgList[this.index].style.display = "block";
+        TweenMax.to(imgList[this.index] , 2, {x: 800} )
+        }
   
     cycleRight () {
-      this.imgList[this.index].style.display = "none";
-      this.index == this.imgList.length - 1
-        ? this.index = 0
-        : this.index ++;
-      this.imgList[this.index].style.display = "block";
+        this.imgList[this.index].style.display = "none";
+        this.index == this.imgList.length - 1
+            ? this.index = 0
+            : this.index ++;
+        this.imgList[this.index].style.display = "block";
     }
     
   }

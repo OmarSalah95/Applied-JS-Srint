1528af141aa93e9c89701f94251beffb27bb1cf0
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-01 09:19:09 -0800

    MVP is done basically just working on a final bug.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,19 +1,28 @@
 class TabLink {
   constructor(tabElement){
-    
+    this.tabElement = tabElement
+    this.data = tabElement.dataset.tab;
+    this.data == 'all'
+      ?this.cards = document.querySelectorAll('.card')
+      :this.cards = document.querySelectorAll(`.card[data-tab='${this.data}']`);
+    Array.from(this.cards).map(card => new TabCard(card));
+    this.tabElement.addEventListener("click", e => this.selectTab());
   }
 
   selectTab(){
-
+  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('.active-tab'))
+  this.cards = document.querySelectorAll('.card').forEach(card => card.style.display = 'none')
+  this.tabElement.classList.add('.active-tab')
+  this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
-    
+    this.cardElement = cardElement
   }
   selectCard(){
-    
+    this.cardElement.style.display = 'flex'
   }
 
 }
